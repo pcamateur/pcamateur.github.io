9d6e08f91eb01e14609623b77718f2453d1a765e
pcamateur <pcamateur@outlook.com>
2019-08-23 18:31:47 +0800

    4 count

--- count/c.js
+++ count/c.js
@@ -1,16 +1,16 @@
 var oCount = document.getElementById("count");
 var oSpans = document.getElementsByTagName("span");
 
-var endTime = new Date('2019/08/25 09:10:33');
+var endTime = new Date('2019/08/25 00:00:00');
 
 var timer = null;
 
 clearInterval(timer);
-timer = setInterval(countTime,1000);
+timer = setInterval(countTime, 1000);
 
-function countTime(){
+function countTime() {
     var nowTime = new Date();
-    var lastTime = parseInt((endTime - nowTime)/1000);
+    var lastTime = parseInt((endTime - nowTime) / 1000);
 
     if (lastTime >= 0) {
         var day = parseInt(lastTime / 60 / 60 / 24);
@@ -22,12 +22,7 @@ function countTime(){
         oSpans[1].innerHTML = hour;
         oSpans[2].innerHTML = min;
         oSpans[3].innerHTML = sec;
-    }
-
-    else {
+    } else {
         clearInterval(timer);
     }
 }
-
-
-
---
+++ count/f.js
@@ -0,0 +1,28 @@
+var oCount3 = document.getElementById("count3");
+var oSpans3 = document.getElementsByTagName("span");
+
+var endTime3 = new Date('2019/11/14 00:00:00');
+
+var timer3 = null;
+
+clearInterval(timer3);
+timer3 = setInterval(countTime3, 1000);
+
+function countTime3() {
+    var nowTime3 = new Date();
+    var lastTime3 = parseInt((endTime3 - nowTime3) / 1000);
+
+    if (lastTime3 >= 0) {
+        var day3 = parseInt(lastTime3 / 60 / 60 / 24);
+        var hour3 = parseInt(lastTime3 / 60 / 60 % 24);
+        var min3 = parseInt(lastTime3 / 60 % 60);
+        var sec3 = parseInt(lastTime3 % 60);
+
+        oSpans3[12].innerHTML = day3;
+        oSpans3[13].innerHTML = hour3;
+        oSpans3[14].innerHTML = min3;
+        oSpans3[15].innerHTML = sec3;
+    } else {
+        clearInterval(timer3);
+    }
+}
