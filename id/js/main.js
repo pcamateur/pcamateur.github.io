30c5c0fba9011ef2a180af6220865b5ec5696392
pcamateur <pcamateur@outlook.com>
2022-09-08 01:10:07 +0800

    copied from jst

--- id/js/main.js
+++ id/js/main.js
@@ -33,18 +33,174 @@ function security_Check(id) {
     }
 
     return value;
-} 
+}
 
+function idProccess(data) {
+    const arr1 = new Array();
+    const arr2 = new Array();
+    let ids = data;
+
+    let xsl = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+
+    for(let i = 0; i < ids.length; i++) {
+        arr1.push(parseInt(ids[i]));
+    }
+    for(let j = 0; j < xsl.length; j++) {
+        arr2.push(arr1[j] * xsl[j]);
+    }
+    let temp_value = 0;
+    for(let k = 0; k < arr2.length; k++) {
+        temp_value += arr2[k];
+    }
+
+    let temp_value1 = temp_value % 11;
+
+    const arr3 = [1, 0, "x", 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+    let values = arr3[temp_value1];
+
+    return ids + values;
+}
+
+function sexProccess(data) {
+    let sex_value = data.slice(-1);
+
+    if(sex_value % 2 == 0) {
+        return 'Female';
+    } else {
+        return "Male";
+    }
+}
+
+function bdProccess(data) {
+    let years, month, days;
+    years = data.slice(6, 10);
+    month = data.slice(10, 12);
+    days = data.slice(12, 14);
+
+    return `${years} - ${month} - ${days}`;
+}
+
+function localProccess(datas, jf) {
+    let query_source = datas.slice(0, 6);
+    let pr, ci;
+
+    pr = query_source.slice(0, 2) + "0000";
+    let provice = jf[pr];
+
+    ci = query_source.slice(0, 4) + "00";
+    let city = jf[ci];
+
+    let district = jf[query_source];
+
+    return provice + city + district;
+}
+
+function constellationProccess(datas) {
+    const constellation_list = [
+        'Capricorn',
+        'Aquarius',
+        'Pisces',
+        'Aries',
+        'Taurus',
+        'Gemini',
+        'Cancer',
+        'Leo',
+        'Virgo',
+        'Libra',
+        'Scorpio',
+        'Sagittarius'
+    ]
+
+    const constellation_days = [
+        [1, 20],
+        [2, 19],
+        [3, 21],
+        [4, 21],
+        [5, 21],
+        [6, 22],
+        [7, 23],
+        [8, 23],
+        [9, 23],
+        [10, 23],
+        [11, 23],
+        [12, 23]
+    ]
+
+    let months = parseInt(datas.slice(10, 12));
+    let days = parseInt(datas.slice(12, 14));
+
+    let birthday = [months, days];
+
+    for(let i = 0; i < constellation_days.length; i++) {
+        if(birthday < constellation_days[i]) {
+            var temps1 = constellation_days.indexOf(constellation_days[i]);
+        }
+    }
+
+    return constellation_list[temps1];
+}
+
+function zodiacSignsProccess(datas) {
+    zodiac_signs_list = [
+        'Monkey',
+        'Rooster',
+        'Dog',
+        'Pig',
+        'Rat',
+        'Cattle',
+        'Tiger',
+        'Rabbit',
+        'Dragon',
+        'Snake',
+        'Horse',
+        'Goat'
+    ]
+
+    let years = parseInt(datas.slice(6, 10));
+    let values = years % 12;
+
+    return zodiac_signs_list[values];
+}
+
+function ages(datas) {
+    let now = new Date();
+    let nowYears = now.getFullYear();
+    let nowMonth = now.getMonth() + 1;
+    let nowDay = now.getDate();
+
+    let bYears = parseInt(datas.slice(6, 10));
+    let bMonth = parseInt(datas.slice(10, 12));
+    let bDay = parseInt(datas.slice(12, 14));
+
+
+    if(nowMonth < bMonth) {
+        return (parseInt(nowYears) - parseInt(bYears)) - 1;
+    } else if(nowMonth == bMonth && nowDay < bDay) {
+        return (parseInt(nowYears) - parseInt(bYears)) - 1;
+    } else if(nowMonth == bMonth && nowDay >= bDay) {
+        return parseInt(nowYears) - parseInt(bYears);
+    } else {
+        return parseInt(nowYears) - parseInt(bYears);
+    }
+
+}
 
 function test_show() {
     document.getElementById('alerts-m').innerHTML = '';
     document.getElementById('showings').innerHTML = "";
     // document.getElementById('showings').innerHTML = security_Check(getId());
 
-    if(document.getElementById('showings').innerHTML = security_Check(getId()) == undefined) {
+    if(idProccess(security_Check(getId())) === undefined) {
         document.getElementById('showings').innerHTML = "Error!";    
     } else {
-        document.getElementById('showings').innerHTML = security_Check(getId())
+        document.getElementById('showings').innerHTML = idProccess(security_Check(getId()));
+        document.getElementById('show-sex').innerHTML = "Sexual: " + sexProccess(security_Check(getId()));
+        document.getElementById('show-birthday').innerHTML = "Birthday: " + bdProccess(security_Check(getId()));
+        document.getElementById('show-address').innerHTML = "Address: " + localProccess(security_Check(getId()), datas);
+        document.getElementById('show-constellation').innerHTML = "Constellation: " + constellationProccess(security_Check(getId()));
+        document.getElementById('show-zodiac').innerHTML = "Zodiac Signs: " + zodiacSignsProccess(security_Check(getId()));
+        document.getElementById('show-age').innerHTML = "Age: " + ages(security_Check(getId()));
     }
 }
 
--- id/js/test.js
+++ id/js/test.js
@@ -1,27 +1,156 @@
-var numbers = "21010219550102123";
-const obj = new Object();
-obj['210'] = '三五零';
+// const arr1 = new Array();
+// const arr2 = new Array();
+// let ids = "21010219830825181";
+//
+// let xsl = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+// for(let i = 0; i < ids.length; i++) {
+//     arr1.push(parseInt(ids[i]));
+// }
+// for(let j = 0; j < xsl.length; j++) {
+//     arr2.push(arr1[j] * xsl[j]);
+// }
+// let temp_value = 0;
+// for(let k = 0; k < arr2.length; k++) {
+//     temp_value += arr2[k];
+// }
+//
+// temp_value1 = temp_value % 11;
+//
+// arr3 = [1, 0, "x", 9, 8, 7, 6, 5, 4, 3, 2, 1];
+//
+// values = arr3[temp_value1];
+//
+// console.log(ids + values);
 
-// if(obj.hasOwnProperty(numbers.slice(0, 3))) {
-//     console.log("OK!");
+
+
+// const arrs = new Array();
+// for(let a = 1; a <= 10; a++) {
+//     arrs.push(a);
+// }
+// console.log(arrs);
+// let tvalue = 0;
+// for(let b = 1; b < arrs.length; b += 2) {
+//     tvalue = arrs[b - 1] + arrs[b] + tvalue;
+// }
+// console.log(tvalue);
+// let tv2 = 0;
+// for(let c = 0; c < arrs.length; c++) {
+//     tv2 += arrs[c]
+// }
+// console.log(tv2);
+
+// const arra = new Array();
+// for(let d = 1; d < 17; d++) {
+//     arra.push(d);
+// }
+//
+// console.log(arra);
+// console.log(arra.slice(-1));
+
+
+// const arra = [[2, 19], [3, 16], [3, 17], [3, 18], [3, 19]];
+// const arrb = [3, 18];
+//
+// for(let i = 0; i < arra.length; i++) {
+//     if(arrb < arra[i]) {
+//         console.log(arra.indexOf(arra[i]));
+//     }
 // }
 
-// console.log(numbers.slice(0, 4));
-
-// if(!(/(^\d{17}$)/.test(numbers))) {
-//     console.log("KKKs!");
-// } 
-
-// if(!(/(^\d{18}$)/.test(parseInt(numbers)))) {
-//     console.log("SSSk!");
+// function constellationProccess(datas) {
+//     const constellation_list = [
+//         'Capricorn',
+//         'Aquarius',
+//         'Pisces',
+//         'Aries',
+//         'Taurus',
+//         'Gemini',
+//         'Cancer',
+//         'Leo',
+//         'Virgo',
+//         'Libra',
+//         'Scorpio',
+//         'Sagittarius'
+//     ]
+//
+//     const constellation_days = [
+//         [1, 20],
+//         [2, 19],
+//         [3, 21],
+//         [4, 21],
+//         [5, 21],
+//         [6, 22],
+//         [7, 23],
+//         [8, 23],
+//         [9, 23],
+//         [10, 23],
+//         [11, 23],
+//         [12, 23]
+//     ]
+//
+//     let months = parseInt(datas.slice(10, 12));
+//     let days = parseInt(datas.slice(12, 14));
+//
+//     let birthday = [months, days];
+//
+//     for(let i = 0; i < constellation_days.length; i++) {
+//         if(birthday < constellation_days[i]) {
+//             var temps1 = constellation_days.indexOf(constellation_days[i]);
+//         }
+//     }
+//
+//     return constellation_list[temps1];
 // }
+//
+// console.log(constellationProccess('21010219830825181'));
 
-// if((/(^\d{18}$)/.test(numbers))) {
-//     console.log("That's a numbers!");
+// function zodiacSignsProccess(datas) {
+//     zodiac_signs_list = [
+//         'Monkey',
+//         'Rooster',
+//         'Dog',
+//         'Pig',
+//         'Rat',
+//         'Cattle',
+//         'Tiger',
+//         'Rabbit',
+//         'Dragon',
+//         'Snake',
+//         'Horse',
+//         'Goat'
+//     ]
+//
+//     let years = parseInt(datas.slice(6, 10));
+//     let values = years % 12;
+//
+//     return zodiac_signs_list[values];
+//
 // }
+//
+// console.log(zodiacSignsProccess('21010219830825181'));
 
-// if((/(^\d{18}$)/.test(parseInt(numbers)))) {
-//     console.log("That's a string!");
-// }
+function ages(datas) {
+    let now = new Date();
+    let nowYears = now.getFullYear();
+    let nowMonth = now.getMonth() + 1;
+    let nowDay = now.getDate();
 
-console.log(numbers.slice(10, 11));
+    let bYears = parseInt(datas.slice(6, 10));
+    let bMonth = parseInt(datas.slice(10, 12));
+    let bDay = parseInt(datas.slice(12, 14));
+
+
+    if(nowMonth < bMonth) {
+        return (parseInt(nowYears) - parseInt(bYears)) - 1;
+    } else if(nowMonth == bMonth && nowDay < bDay) {
+        return (parseInt(nowYears) - parseInt(bYears)) - 1;
+    } else if(nowMonth == bMonth && nowDay >= bDay) {
+        return parseInt(nowYears) - parseInt(bYears);
+    } else {
+        return parseInt(nowYears) - parseInt(bYears);
+    }
+
+}
+
+console.log(ages("21010219830925181"));
